dd2dfc9ff8cac7c7f71ee9bdd3b5a193551fd494
LegiaminhGitHud <legiaminhoffice@gmail.com>
2024-02-08 12:00:36 +0700

    debug database

--- api/getplayer.js
+++ api/getplayer.js
@@ -1,15 +1,34 @@
 const mysql = require('mysql2');
-;
-const user_input  = "john"
+
+try{
+    const user_input  = "john"
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'Legiaminh2010',
   database: 'leaderboard_cps'
 })
+}
+catch{
+    throw(error)
+}
+
 module.exports = (req, res) => {
     res.send("hello welcome to the server")
-
+    try{
+        const user_input  = "john"
+        const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'Legiaminh2010',
+      database: 'leaderboard_cps'
+    })
+    }
+    catch{
+        res.send("theres a problem with teh db")
+        throw(error)
+    }
+    
     try{
         connection.query('SELECT * FROM leaderboard_cps.test', (err, results) => {
             if (err) {
