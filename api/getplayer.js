807ad19d81111cfaccaf4865263e5d6ff03fefd7
LegiaminhGitHud <legiaminhoffice@gmail.com>
2024-02-08 12:02:05 +0700

    debug

--- api/getplayer.js
+++ api/getplayer.js
@@ -1,50 +1,17 @@
 const mysql = require('mysql2');
-
-try{
-    const user_input  = "john"
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'Legiaminh2010',
   database: 'leaderboard_cps'
-})
-}
-catch{
-    throw(error)
-}
+});
 
 module.exports = (req, res) => {
-    res.send("hello welcome to the server")
-    try{
-        const user_input  = "john"
-        const connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'Legiaminh2010',
-      database: 'leaderboard_cps'
-    })
-    }
-    catch{
-        res.send("theres a problem with teh db")
-        throw(error)
-    }
-    
-    try{
-        connection.query('SELECT * FROM leaderboard_cps.test', (err, results) => {
-            if (err) {
-              return res.status(500).json({ error: 'Database query failed' });
-            }
-            else{            
-                res.send("connected to the database successfully")
-            }
-
-        });
-        
-    }
-    catch{
-        res.send("theres a problem")
-    }
-//   });
+    pool.query('SELECT * FROM leaderboard_cps.test', (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Database query failed' });
+        }
+        res.send("Connected to the database successfully");
+    });
 };
-
-
