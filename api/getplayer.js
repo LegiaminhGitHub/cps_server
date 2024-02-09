33b74bbeae80b7efe8f68f2adcc432d58f612f4a
LegiaminhGitHud <legiaminhoffice@gmail.com>
2024-02-09 10:48:45 +0700

    legiaminh

--- api/getplayer.js
+++ api/getplayer.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2');
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Legiaminh2010',
-  database: 'leaderboard_cps'
+const pool  = mysql.createPool({
+  host: 'sql108.infinityfree.com',
+  user: 'if0_35940057',
+  password: 'iuUaS2qActscHxo',
+  database: 'if0_35940057_XXX',
+  port: 3306
 });
+
 
 module.exports = (req, res) => {
     pool.query('SELECT * FROM leaderboard_cps.test', (err, results) => {
