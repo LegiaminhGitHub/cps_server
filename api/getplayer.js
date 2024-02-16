a49ab586240936441731da20cf284bc388e66007
LegiaminhGitHud <legiaminhoffice@gmail.com>
2024-02-16 17:43:22 +0700

    final test

--- api/getplayer.js
+++ api/getplayer.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql2');
-// const pool = mysql.createPool({
-//     host: "sql6.freesqldatabase.com",
-//     user: 'sql6684534',
-//     password: '3V5Yxi9YXk',
-//     database: 'sql6684534',
-//     port: 3306,
-// });
+const pool = mysql.createPool({
+    host: "sql6.freesqldatabase.com",
+    user: 'sql6684534',
+    password: '3V5Yxi9YXk',
+    database: 'sql6684534',
+    port: 3306,
+});
 
 module.exports = (req, res) => {
     res.send("welcome to the server")
-    // pool.query('SELECT * FROM leaderboard_cps.test', (err, results) => {
-    //     if (err) {
-    //         console.error(err);
-    //         return res.status(500).json({ error: 'Database query failed', details: err });
-    //     }
-    //     res.send("Connected to the database successfully");
-    // });
+    pool.query('SELECT * FROM leaderboard_cps.test', (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Database query failed', details: err });
+        }
+        res.send("Connected to the database successfully");
+    });
 };
