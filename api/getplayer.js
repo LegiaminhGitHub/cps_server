b35289a494198ad3d838b610461998958ebfe232
LegiaminhGitHud <legiaminhoffice@gmail.com>
2024-02-16 17:54:16 +0700

    debug update

--- api/getplayer.js
+++ api/getplayer.js
@@ -8,12 +8,17 @@ const pool = mysql.createPool({
 });
 
 module.exports = (req, res) => {
-    res.send("welcome to the server")
+    var messages = { "message": [] }; // Corrected key name
+
+    messages["message"].push("Welcome to the server");
+
     pool.query('SELECT * FROM leaderboard_cps.test', (err, results) => {
         if (err) {
             console.error(err);
+            messages["message"].push("Cannot connect to the database");
             return res.status(500).json({ error: 'Database query failed', details: err });
         }
-        res.send("Connected to the database successfully");
+        messages["message"].push("Connected to the database successfully");
+        res.send({ messages }); // Send the entire messages object as JSON
     });
 };
