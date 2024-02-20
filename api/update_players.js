591dd553be548c60934ab98780caeb279d6f7e47
LegiaminhGitHud <legiaminhoffice@gmail.com>
2024-02-20 21:40:19 +0700

    firebase migration test

--- api/update_players.js
+++ api/update_players.js
@@ -1,15 +1,52 @@
-const mysql = require('mysql2');
+const admin = require('firebase-admin');
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Legiaminh2010",
-    database: "leaderboard_cps"
+const serviceAccount = require('./click-per-second-web-firebase-adminsdk-8y5vt-50ce8c8b7b.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://click-per-second-web-default-rtdb.asia-southeast1.firebasedatabase.app',
 });
+
+// Data to insert
+const myData = {
+  cps: 9.4,
+  score: 36,
+};
+
+// Access the Realtime Database
+const db = admin.database();
+const usersRef = db.ref('users');
+
+async function addUserIfNotExists(userId) {
+  try {
+    const snapshot = await usersRef.child(userId).once('value');
+    const userData = snapshot.val();
+
+    if (!userData) {
+      // User doesn't exist, add new data
+      await usersRef.child(userId).set(myData);
+      console.log(`User ${userId} added`);
+    } else {
+      console.log(`User ${userId} already exists`);
+      try{
+        await usersRef.child(userId).update(myData);
+        console.log("user updated")
+      }
+      catch(error){
+        console.log(`theres a problem with the update please try again later \n ${err}`)
+      }
+    }
+  } catch (error) {
+    console.error('Error checking or adding user:', error);
+  }
+}
+
+// Example usage:
+// addUserIfNotExists('mike'); // Replace with the actual user ID
+
 
 module.exports = (req, res) => {
   var data = req.body;
   // You can use the 'data' variable to update your database here
-  res.send({message :"welcome to the server !"});
+  res.send({message :`welcome to the server ! ${data}`});
 };
 
