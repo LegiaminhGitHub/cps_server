c00a6e89028db2afa06be1004a58621a53842945
LegiaminhGitHud <legiaminhoffice@gmail.com>
2024-02-21 17:12:55 +0700

    l

--- api/update_players.js
+++ api/update_players.js
@@ -41,26 +41,25 @@ async function addUserIfNotExists(userId) {
 
 
 module.exports = (req, res) => {
-  // var messsages = {"mess" : []}
-  // try{
-  //   messsages["mess"].push("welcome to the server")
-  //   // var data = req.body;
-  //   messsages["mess"].push(data)
-  //   try{  
-  //     const db = admin.database();
-  //     const usersRef = db.ref('users');
-  //     messsages["mess"].push("connected tofirebase server")
-  //   }
-  //   catch(error){
-  //     messsages["mess"].push("connection to firebase failed")
-  //     res.send({messages})
-  //   }
-  //   res.send({messages})
-  //   // You can use the 'data' variable to update your database here
-  // }
-  // catch(error){
-  //   res.send(error)
-  // }
-  res.send("welcome bro")
+  var messsages = {"mess" : []}
+  try{
+    messsages["mess"].push("welcome to the server")
+    // var data = req.body;
+    messsages["mess"].push(data)
+    try{  
+      const db = admin.database();
+      const usersRef = db.ref('users');
+      messsages["mess"].push("connected tofirebase server")
+    }
+    catch(error){
+      messsages["mess"].push("connection to firebase failed")
+      res.send(messsages)
+    }
+    res.send(messages)
+    // You can use the 'data' variable to update your database here
+  }
+  catch(error){
+    res.send(error)
+  }
 };
 
