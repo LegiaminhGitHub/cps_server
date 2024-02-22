618c680fddd722d5646cb2b1bf42259e9d5444bf
LegiaminhGitHud <legiaminhoffice@gmail.com>
2024-02-22 08:50:18 +0700

    lol

--- api/update_players.js
+++ api/update_players.js
@@ -15,48 +15,36 @@ async function addUserIfNotExists(userId) {
     if (!userData) {
       // User doesn't exist, add new data
       await usersRef.child(userId).set(myData);
-      console.log(`User ${userId} added`);
-      messages["mess"].push(`User ${userId} added`)
+      messages["mess"].push(`User ${userId} added`);
     } else {
-      console.log(`User ${userId} already exists`);
-      messages["mess"].push(`User ${userId} already exists`)
-      try{
+      messages["mess"].push(`User ${userId} already exists`);
+      try {
         await usersRef.child(userId).update(myData);
-        console.log("user updated")
-        messages["mess"].push("user updated")
-      }
-      catch(error){
-        console.log(`theres a problem with the update please try again later \n ${err}`)
+        messages["mess"].push("User updated");
+      } catch (error) {
+        messages["mess"].push(`Error updating user data: ${error.message}`);
       }
     }
   } catch (error) {
-    console.error('Error checking or adding user:', error);
+    messages["mess"].push("Connection to Firebase failed");
+    messages["mess"].push(`Error: ${error.message}`);
   }
 }
-
 module.exports = async (req, res) => {
-  // Initialize the messages object
-
   try {
     messages["mess"].push("Welcome to the server");
     var data = req.body; // Uncomment this line if you have data from the request body
     messages["mess"].push(data); // Assuming data is defined elsewhere
 
-    try {
-      const db = admin.database();
-      const usersRef = db.ref('users');
-      messages["mess"].push("Connected to Firebase server")
-      addUserIfNotExists("mike"); // Successfully connected
-    } catch (error) {
-      messages["mess"].push("error:" , error)
-      messages["mess"].push("Connection to Firebase failed");
-      addUserIfNotExists("mike"); // Only call this once
-      res.json(messages);
-      return;
-    }
-    
-    res.send(messages); // Send the populated messages object
+    const db = admin.database();
+    const usersRef = db.ref('users');
+    await addUserIfNotExists("mike")
+    messages["mess"].push("Connected to Firebase server");
+   ; // Successfully connected
+
+    res.json(messages); // Send the populated messages object
   } catch (error) {
-    res.send(error); // Handle other errors
+    console.error(`Error handling request: ${error.message}`);
+    res.status(500).send("Internal server error");
   }
-}
+};
