4a883db132d70afc36b9557022a665dc12b6c7dd
LegiaminhGitHud <legiaminhoffice@gmail.com>
2024-03-03 22:11:18 +0700

    l

--- api/update_players.js
+++ api/update_players.js
@@ -1,60 +1,58 @@
-
 const { MongoClient } = require('mongodb');
-require("dotenv").config();
+require('dotenv').config({ path: './api/.env' });
+
 const myData = {
   cps: 9.4,
   score: 36,
 };
 
-let messages = {"mess" : []} 
+let messages = { "mess": [] };
+let client; // Declare the client variable at a higher scope
 
 async function connectToMongoDB() {
-
   try {
     const uri = process.env.MGDB_URI;
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    
-    await client.connect();
-    messages["mess"].push('Connected to MongoDB successfully!')
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    // Export the connected client for use in other API functions
-    return client;
+    await client.connect();
+    messages["mess"].push('Connected to MongoDB successfully!');
   } catch (error) {
-    messages["mess"].push(`Error connecting to MongoDB:${error}`)
+    messages["mess"].push(`Error connecting to MongoDB: ${error}`);
     throw error;
   }
 }
 
 async function log_db() {
   try {
-    const client = await connectToMongoDB(); // Connect to MongoDB first
+    await connectToMongoDB(); // Connect to MongoDB first
     const db = client.db("cps-leaderboard");
     const collection = db.collection("user-data");
 
     const query = { name: "legiaminh" };
-    const updateOperation = { $set: myData }; // Update the "cps" and "score" fields
+    const updateOperation = { $set: myData };
 
-    const result = await collection.updateOne(query, updateOperation); // Perform the update
+    const result = await collection.updateOne(query, updateOperation);
 
     if (result.modifiedCount > 0) {
-      messages["mess"].push('Document updated successfully!')
-      
+      messages["mess"].push('Document updated successfully!');
     } else {
-      await collection.insertOne(myData); // Insert a new document if not found
-      messages["mess"].push('New document inserted successfully!')
+      await collection.insertOne(myData);
+      messages["mess"].push('New document inserted successfully!');
     }
-  } catch (error) {;
-    messages["mess"].push(`Error adding/updating data:, ${error}`)
+  } catch (error) {
+    messages["mess"].push(`Error adding/updating data: ${error}`);
   } finally {
-    await client.close(); // Close the MongoDB connection
+    if (client) {
+      await client.close(); // Close the MongoDB connection
+    }
   }
 }
 
 module.exports = async (req, res) => {
-  try{
-    await log_db()
-  }
-  catch{
-    res.json(messages)
+  try {
+    await log_db();
+    res.json(messages);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
   }
 };
